b3d118e97660acc95bdc60195c3fa2a24ceb917e
bored-arvi <aravinth3007k@gmail.com>
2025-12-15 23:51:35 +0530

    Initialize Spring Boot application entry point

---
+++ src/main/java/com/gdg/canteen/CanteenApplication.java
@@ -0,0 +1,18 @@
+package com.gdg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for College S-Canteen Menu Management API
+ * 
+ * @author Aravinth 
+ * @version 1.0
+ */
+@SpringBootApplication
+public class CanteenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanteenApplication.class, args);
+    }
+}
